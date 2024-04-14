6680787e696b82456afc9562a693be7700d203c2
Shruti Nath <shruti280502@gmail.com>
2024-04-14 22:49:58 +0530

    update

--- Weather/src/App.js
+++ Weather/src/App.js
@@ -1,7 +1,6 @@
 // import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-// import DoseListCardBody from "./HomePage/sample";
 import Homepage from './HomePage/Homepage';
 import Page from "./WeatherPage/WeatherPage";
 
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />}/>
         <Route path="/page" element={<Page />} />
-        {/* <Route path="/sample" element={<DoseListCardBody />} /> */}
       </Routes>
     </BrowserRouter>
   )
